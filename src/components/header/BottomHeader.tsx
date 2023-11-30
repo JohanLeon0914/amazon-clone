5c32218437f9170852670fb986d0d14555abdceb
Johan León <64605783+JohanLeon0914@users.noreply.github.com>
2023-11-30 14:24:19 -0500

    success page y agregando funcionalidad de sign out

--- src/components/header/BottomHeader.tsx
+++ src/components/header/BottomHeader.tsx
@@ -1,6 +1,17 @@
+"use client"
 import { LuMenu } from "react-icons/lu";
+import { signOut } from "next-auth/react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeUser } from "@/store/nextSlice";
+import { StateProps } from "../../../types";
 
 const BottomHeader = () => {
+  const dispatch = useDispatch();
+  const { userInfo } = useSelector((state: StateProps) => state.next);
+  const handleSignOut = () => {
+    signOut();
+    dispatch(removeUser());
+  };
   return (
     <div className="w-full h-10 bg-amazon_light text-sm text-white px-4 flex items-center">
       <p className="flex items-center gap-1 h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300">
@@ -21,9 +32,14 @@ const BottomHeader = () => {
       <p className="hidden md:inline-flex items-center h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300">
         Sell
       </p>
-      <p className="hidden md:inline-flex items-center h-8 px-2 border border-transparent hover:border-red-600 hover:text-red-400 text-amazon_yellow cursor-pointer duration-300">
-        Sign out
-      </p>
+      {userInfo && (
+        <button
+          onClick={handleSignOut}
+          className="hidden md:inline-flex items-center h-8 px-2 border border-transparent hover:border-red-600 hover:text-red-400 text-amazon_yellow cursor-pointer duration-300"
+        >
+          Sign Out
+        </button>
+      )}
     </div>
   );
 };
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -6,25 +6,3 @@ export const getProducts = async () => {
     }
     return res.json()
 };
-
-// export const getTrendingProducts = async () => {
-//     const res = await fetch(
-//         process.env.API_TRENDING_PRODUCTS!
-//     );
-//     if (!res.ok) {
-//         throw new Error("Faild to fetch products");
-//     }
-//     return res.json();
-// };
-
-// export const calculatePercentage = (oldPrice: any, price: any) => {
-//     return !!parseFloat(price) && !!parseFloat(oldPrice)
-//       ? (100 - (oldPrice / price) * 100).toFixed(0)
-//       : 0;
-//   };
-  
-//   export const getSingleProudct = async(_id: number) => {
-//     const products:Product[] = await getProducts()
-//     const item = products.find((product) => product._id === _id);
-//     return item;
-//   };
